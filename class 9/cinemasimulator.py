9d0787e8d17d9f10c075f4659a99e0bb8429cd3f
Kanishka <kanishka08np@gmail.com>
2020-10-15 18:08:10 +0400

    test

--- class 9/cinemasimulator.py
+++ class 9/cinemasimulator.py
@@ -1,23 +1,11 @@
+
 film = {"Shrek": {"age": 3, "seats": 6},
-       "Mission Impossible 2": {"age": 5, "seats": 3},
-       "Endgame": {"age": 7, "seats": 5},
-       "Infinity War": {"age": 9, "seats": 5}}
-
-
-
-
-
+        "Mission Impossible 2": {"age": 5, "seats": 3},
+        "Endgame": {"age": 7, "seats": 5},
+        "Infinity War": {"age": 9, "seats": 5}}
 
 choice = input(
-   "Which film would you like to watch? "
-   "\n Shrek \n Mission Impossible 2 \n "
-   "Endgame \n Infinity War "
-   "\n Press enter to exit : ").strip().title()
-if choice in film:
-    print("welcome")
-else:
-    print("sorry your choice is not in the list")
-
-
-
-
+    "Which film would you like to watch? "
+    "\n Shrek \n Mission Impossible 2 \n "
+    "Endgame \n Infinity War "
+    "\n Press enter to exit : ").strip().title()
